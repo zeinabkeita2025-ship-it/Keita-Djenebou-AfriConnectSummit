dc1fadaf31ab2cc910769c4209aa7a888c99a1cf
Djénébou Keita <zeinabkeita2025@gmail.com>
2026-07-20 18:25:34 +0100

    Validation , Comentaire du code

---
+++ README.md
@@ -0,0 +1,165 @@
+# 🌍 AfriConnect Summit 2026
+
+Bienvenue sur **AfriConnect Summit 2026**, un site web événementiel moderne conçu pour promouvoir un sommet dédié à l'innovation, à la technologie et à l'entrepreneuriat en Afrique.
+
+Le projet a été réalisé entièrement avec **HTML5**, **CSS3** et **JavaScript**, sans framework, dans le but de mettre en pratique les compétences en développement web front-end.
+
+---
+
+## 📌 Présentation
+
+AfriConnect Summit est une conférence fictive qui rassemble :
+
+- 🚀 Entrepreneurs
+- 💻 Développeurs
+- 🤖 Experts en Intelligence Artificielle
+- 📊 Data Scientists
+- 🎨 Designers UX/UI
+- 🌍 Investisseurs et acteurs du numérique
+
+Le site permet aux visiteurs de découvrir l'événement, consulter le programme, les intervenants et s'inscrire.
+
+---
+
+## ✨ Fonctionnalités
+
+- 🏠 Page d'accueil moderne
+- 📅 Programme interactif sur 3 jours
+- 👨‍💼 Présentation des intervenants
+- 📞 Formulaire d'inscription avec validation JavaScript
+- 🌙 Mode sombre (Dark Mode)
+- ⏳ Compteur dynamique avant l'événement
+- 🔍 Navigation responsive
+- 📱 Compatible mobiles, tablettes et ordinateurs
+- ⬆️ Bouton "Retour en haut"
+- 🎬 Vidéo de présentation
+- 🎨 Animations CSS et JavaScript
+
+---
+
+## 🛠️ Technologies utilisées
+
+- HTML5
+- CSS3
+- JavaScript (ES6)
+- Bootstrap Icons
+- Google Fonts (Poppins)
+
+---
+
+## 📂 Structure du projet
+
+```text
+AfriConnectSummit/
+│
+├── index.html
+├── programme.html
+├── intervenants.html
+├── contact.html
+│
+├── css/
+│   └── style.css
+│
+├── js/
+│   └── main.js
+│
+├── images/
+│   ├── ...
+│  
+└── README.md
+```
+
+---
+
+## 📄 Description des pages
+
+### 🏠 Accueil
+
+- Présentation de l'événement
+- Compteur
+- Boutons d'action
+- Sponsors
+
+### 📅 Programme
+
+- Planning des trois journées
+- Système d'onglets interactifs
+- Tableau des conférences
+
+### 👨‍💼 Intervenants
+
+- Présentation des principaux speakers
+- Cartes interactives
+- Informations professionnelles
+
+### 📞 Contact
+
+- Formulaire d'inscription
+- Validation JavaScript
+- Messages d'erreur personnalisés
+
+---
+
+## 🎯 Objectifs du projet
+
+Ce projet a été réalisé afin de :
+
+- maîtriser HTML5 sémantique ;
+- créer une interface responsive ;
+- manipuler le DOM avec JavaScript ;
+- appliquer les bonnes pratiques CSS ;.
+
+---
+
+## 🚀 Visionnage
+
+1. Le lien du depot github :
+
+```bash
+https://github.com/zeinabkeita2025-ship-it/Keita-Djenebou-AfriConnectSummit.git
+```
+
+2. le lien fonctionnel du site
+
+```https://zeinabkeita2025-ship-it.github.io/Keita-Djenebou-AfriConnectSummit/
+
+```
+---
+
+## 📱 Responsive Design
+
+Le site est optimisé pour :
+
+- 📱 Smartphones
+- 📲 Tablettes
+- 💻 Ordinateurs
+
+---
+
+## 📸 Aperçu
+
+Le site comprend :
+
+- Hero moderne
+- Programme interactif
+- Cartes des intervenants
+- Formulaire dynamique
+- Footer professionnel
+
+---
+
+## 🔮 Améliorations futures
+
+- Authentification des utilisateurs
+- Base de données
+- Paiement des billets
+- Carte interactive du lieu
+- Galerie photos
+- Blog
+- Multilingue (Français / Anglais)
+
+---
+
+## 👨‍💻 Auteur
+
+Projet réalisé par **[Keita-Djenebou]** dans le cadre d'un projet de développement web.
--- js/main.js
+++ js/main.js
@@ -8,41 +8,41 @@ hamburger.addEventListener("click", () => {
 });
 // Dark Mode 
 const body = document.body;
-    // la selection du bouton dark-light
-    const toggleBtn = document.getElementById("themeToggle");
-    if (toggleBtn) { //verifie le bouton 
-        let savedTheme = localStorage.getItem("theme") || "light";//on recupère le theme sauvegardé
-        body.className = savedTheme;
-        toggleBtn.textContent = savedTheme === "dark" ? "⭐" : "🌙";
-        toggleBtn.addEventListener("click", () => { // quand on clique sur le bouton
-            if (body.classList.contains("dark")) {
-                body.classList.replace("dark", "light");
-                localStorage.setItem("theme", "light");
-                toggleBtn.textContent = "🌙";
-            } else {
-                body.classList.replace("light", "dark");
-                localStorage.setItem("theme", "dark");
-                toggleBtn.textContent = "⭐";
-            }
-        });
-    }
+// la selection du bouton dark-light
+const toggleBtn = document.getElementById("themeToggle");
+if (toggleBtn) { //verifie le bouton 
+    let savedTheme = localStorage.getItem("theme") || "light";//on recupère le theme sauvegardé
+    body.className = savedTheme;
+    toggleBtn.textContent = savedTheme === "dark" ? "⭐" : "🌙";
+    toggleBtn.addEventListener("click", () => { // quand on clique sur le bouton
+        if (body.classList.contains("dark")) {
+            body.classList.replace("dark", "light");
+            localStorage.setItem("theme", "light");
+            toggleBtn.textContent = "🌙";
+        } else {
+            body.classList.replace("light", "dark");
+            localStorage.setItem("theme", "dark");
+            toggleBtn.textContent = "⭐";
+        }
+    });
+}
 //Navbar qui change de style au scroll
 const navbar = document.querySelector(".navbar");
-     if (navbar) { //dectecte la position du navbar
-        window.addEventListener("scroll", () => {  //quand on commence à scroller la page
-            navbar.classList.toggle("scrolled", window.scrollY > 40);//gere le style au moment du scroll et dans le cas contraire(quand on scroll de 40px)
-        });
-    }
- //Bouton retour en haut
-  const btn = document.getElementById("btnTop");
-    if (btn) { // detecte le bouton (sa position)
-        window.addEventListener("scroll", () => { // quand on scroll la page
-            btn.style.display = window.scrollY > 30 ? "block" : "none";
-        });
-        btn.addEventListener("click", () => { // quand on clique sur le bouton
-            window.scrollTo({ top: 0, behavior: 'smooth' }); // ca ramene en haut avec animation
-        });
-    }
+if (navbar) { //dectecte la position du navbar
+    window.addEventListener("scroll", () => {  //quand on commence à scroller la page
+        navbar.classList.toggle("scrolled", window.scrollY > 40);//gere le style au moment du scroll et dans le cas contraire(quand on scroll de 40px)
+    });
+}
+//Bouton retour en haut
+const btn = document.getElementById("btnTop");
+if (btn) { // detecte le bouton (sa position)
+    window.addEventListener("scroll", () => { // quand on scroll la page
+        btn.style.display = window.scrollY > 30 ? "block" : "none";
+    });
+    btn.addEventListener("click", () => { // quand on clique sur le bouton
+        window.scrollTo({ top: 0, behavior: 'smooth' }); // ca ramene en haut avec animation
+    });
+}
 
 // Section en fondu
 const sections = document.querySelectorAll(".fade");
@@ -55,31 +55,31 @@ if (sections.length > 0) {
     sections.forEach(s => fadeObserver.observe(s));
 }
 // Cmpteur animé
- //la selection des elements(chiffres à compter)
-    const counters = document.querySelectorAll('.counter');
-    if (counters.length > 0) { //verifie s'il ya au moins un compteur(carte)
-        const observer = new IntersectionObserver((entries) => { // création d'un observateur qui surveille les éléments
-            entries.forEach(entry => { // on parcourt les éléments à observer
-                if (entry.isIntersecting) {
-                    const el = entry.target; // l'élément html à observer
-                    const target = +el.dataset.target; //Lit la valeur de l'attribut data-target
-                    let count = 0;
-                    const update = () => { //une fonction qui met à jour le nombre affiché 
-                        count += target / 100;// la vitesse du compteur
-                        if (count < target) {
-                            el.textContent = Math.floor(count);
-                            requestAnimationFrame(update);//Demande au navigateur d'exécuter update() à la prochaine image (environ 60 fois par seconde)AF
-                        } else {
-                            el.textContent = target;
-                        }
-                    };
-                    update(); //Lance l'animation du compteur.
-                    observer.unobserve(el); //on arrete d'observer les éléments pour que le compteur ne recommence op
-                }
-            });
-        }, { threshold: 1 }); //quand tout l'élément visible à l'écran(100%)
-        counters.forEach(c => observer.observe(c)); //On demande à l'observateur de surveiller chaque élément .counter.
-    }
+//la selection des elements(chiffres à compter)
+const counters = document.querySelectorAll('.counter');
+if (counters.length > 0) { //verifie s'il ya au moins un compteur(carte)
+    const observer = new IntersectionObserver((entries) => { // création d'un observateur qui surveille les éléments
+        entries.forEach(entry => { // on parcourt les éléments à observer
+            if (entry.isIntersecting) {
+                const el = entry.target; // l'élément html à observer
+                const target = +el.dataset.target; //Lit la valeur de l'attribut data-target
+                let count = 0;
+                const update = () => { //une fonction qui met à jour le nombre affiché 
+                    count += target / 100;// la vitesse du compteur
+                    if (count < target) {
+                        el.textContent = Math.floor(count);
+                        requestAnimationFrame(update);//Demande au navigateur d'exécuter update() à la prochaine image (environ 60 fois par seconde)AF
+                    } else {
+                        el.textContent = target;
+                    }
+                };
+                update(); //Lance l'animation du compteur.
+                observer.unobserve(el); //on arrete d'observer les éléments pour que le compteur ne recommence op
+            }
+        });
+    }, { threshold: 1 }); //quand tout l'élément visible à l'écran(100%)
+    counters.forEach(c => observer.observe(c)); //On demande à l'observateur de surveiller chaque élément .counter.
+}
 //Onglets des tableau  jour et son planing
 const tabs = document.querySelectorAll(".tab");
 const plannings = document.querySelectorAll(".planning");
@@ -106,7 +106,7 @@ tabs.forEach(tab => {
     });
 
 });
-    //Filtrage dynamique des cartes
+//Filtrage dynamique des cartes
 const buttons = document.querySelectorAll(".btn");
 const cards = document.querySelectorAll(".col");
 
@@ -123,23 +123,23 @@ buttons.forEach(button => {
 
             const category = card.querySelector(".speaker").dataset.filter;
 
-            if(filter === "all" || category === filter){
+            if (filter === "all" || category === filter) {
 
                 card.style.display = "block";
 
-                setTimeout(()=>{
+                setTimeout(() => {
                     card.classList.remove("hide");
                     card.classList.add("show");
-                },10);
+                }, 10);
 
-            }else{
+            } else {
 
                 card.classList.remove("show");
                 card.classList.add("hide");
 
-                setTimeout(()=>{
-                    card.style.display="none";
-                },300);
+                setTimeout(() => {
+                    card.style.display = "none";
+                }, 300);
 
             }
 
@@ -147,7 +147,7 @@ buttons.forEach(button => {
 
     });
 
-});  
+});
 
 //Validation de formulaire
 const form = document.getElementById("form");
@@ -166,185 +166,183 @@ const selectpError = document.getElementById("SelectpError");
 const messageError = document.getElementById("messageError");
 
 const succes = document.querySelector(".succes");
-if(form){
-form.addEventListener("submit", function(e){
+if (form) {
+    form.addEventListener("submit", function (e) {
 
-    e.preventDefault();
+        e.preventDefault();
 
-    let valide = true;
+        let valide = true;
 
-    // Réinitialisation
-    succes.textContent = "";
+        // Réinitialisation
+        succes.textContent = "";
 
-    document.querySelectorAll("small").forEach(s=>{
-        if(!s.classList.contains("succes")){
-            s.textContent="";
-        }
-    });
-
-    document.querySelectorAll("input, textarea, select").forEach(champ=>{
-        champ.classList.remove("erreur","valide");
-    });
-
-    // Nom
-
-    if(nom.value.trim().length < 3){
-
-        nameError.textContent="Veuillez entrer votre nom complet.";
-        nom.classList.add("erreur");
-        valide=false;
-
-    }else{
-
-        nom.classList.add("valide");
-
-    }
-
-    // Email
-
-    const regexEmail=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if(!regexEmail.test(email.value.trim())){
-
-        emailError.textContent="Adresse email invalide.";
-        email.classList.add("erreur");
-        valide=false;
-
-    }else{
-
-        email.classList.add("valide");
-
-    }
-
-    // Téléphone
-
-    const regexTel=/^[0-9]{8,15}$/;
-
-    if(!regexTel.test(telephone.value.trim())){
-
-        telephoneError.textContent="Numéro de téléphone invalide.";
-        telephone.classList.add("erreur");
-        valide=false;
-
-    }else{
-
-        telephone.classList.add("valide");
-
-    }
-
-    // Type
-
-    if(type.value===""){
-
-        selectError.textContent="Choisissez un type de participation.";
-        type.classList.add("erreur");
-        valide=false;
-
-    }else{
-
-        type.classList.add("valide");
-
-    }
-
-    // Pays
-
-    if(pays.value===""){
-
-        selectpError.textContent="Choisissez un pays.";
-        pays.classList.add("erreur");
-        valide=false;
-
-    }else{
-
-        pays.classList.add("valide");
-
-    }
-
-    // Motivation
-
-    if(message.value.trim().length < 20){
-
-        messageError.textContent="Votre motivation doit contenir au moins 20 caractères.";
-        message.classList.add("erreur");
-        valide=false;
-
-    }else{
-
-        message.classList.add("valide");
-
-    }
-    // Succès
-    if(valide){
-        succes.textContent=" Inscription effectuée avec succès !";
-        form.reset(); //Après l'envoi vide les champs
-        document.querySelectorAll("input, textarea, select").forEach(champ=>{
-            champ.classList.remove("valide");
+        document.querySelectorAll("small").forEach(s => {
+            if (!s.classList.contains("succes")) {
+                s.textContent = "";
+            }
         });
 
-    }
+        document.querySelectorAll("input, textarea, select").forEach(champ => {
+            champ.classList.remove("erreur", "valide");
+        });
 
-});
+        // Nom
+
+        if (nom.value.trim().length < 3) {
+
+            nameError.textContent = "Veuillez entrer votre nom complet.";
+            nom.classList.add("erreur");
+            valide = false;
+
+        } else {
+
+            nom.classList.add("valide");
+
+        }
+
+        // Email
+
+        const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!regexEmail.test(email.value.trim())) {
+
+            emailError.textContent = "Adresse email invalide.";
+            email.classList.add("erreur");
+            valide = false;
+
+        } else {
+
+            email.classList.add("valide");
+
+        }
+
+        // Téléphone
+
+        const regexTel = /^[0-9]{8,15}$/;
+
+        if (!regexTel.test(telephone.value.trim())) {
+
+            telephoneError.textContent = "Numéro de téléphone invalide.";
+            telephone.classList.add("erreur");
+            valide = false;
+
+        } else {
+
+            telephone.classList.add("valide");
+
+        }
+
+        // Type
+
+        if (type.value === "") {
+
+            selectError.textContent = "Choisissez un type de participation.";
+            type.classList.add("erreur");
+            valide = false;
+
+        } else {
+            type.classList.add("valide");
+        }
+
+        // Pays
+
+        if (pays.value === "") {
+
+            selectpError.textContent = "Choisissez un pays.";
+            pays.classList.add("erreur");
+            valide = false;
+
+        } else {
+
+            pays.classList.add("valide");
+
+        }
+
+        // Motivation
+
+        if (message.value.trim().length < 20) {
+
+            messageError.textContent = "Votre motivation doit contenir au moins 20 caractères.";
+            message.classList.add("erreur");
+            valide = false;
+
+        } else {
+
+            message.classList.add("valide");
+
+        }
+        // Succès
+        if (valide) {
+            succes.textContent = " Inscription effectuée avec succès !";
+            form.reset(); //Après l'envoi vide les champs
+            document.querySelectorAll("input, textarea, select").forEach(champ => {
+                champ.classList.remove("valide");
+            });
+
+        }
+
+    });
 }
 
 //Animation d'apparition des elements de gauche vers droite
 const reveals = document.querySelectorAll(".reveal-left");
-const observers = new IntersectionObserver((entries)=>{
-    entries.forEach(entry=>{
-        if(entry.isIntersecting){
+const observers = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
             entry.target.classList.add("active");
         }
 
     });
-},{
-    threshold:0.15
+}, {
+    threshold: 0.15
 });
 
-reveals.forEach(reveal=>{
+reveals.forEach(reveal => {
     observers.observe(reveal);
 });
 
 //Animation d'apparition des elements de droite vers gauche
 const reveal_right = document.querySelectorAll(".reveal-right");
-const observersdroit = new IntersectionObserver((entries)=>{
-    entries.forEach(entry=>{
-        if(entry.isIntersecting){
+const observersdroit = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
             entry.target.classList.add("active");
         }
 
     });
-},{
-    threshold:0.15
+}, {
+    threshold: 0.15
 });
 
-reveal_right.forEach(reveal=>{
+reveal_right.forEach(reveal => {
     observersdroit.observe(reveal);
 });
 
 //Animations d'apparition des éléments du bas vers le haut
 const reveal_bas = document.querySelectorAll(".reveal-bottom");
-const observerses = new IntersectionObserver((entries)=>{
-    entries.forEach(entry=>{
-        if(entry.isIntersecting){
+const observerses = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
             entry.target.classList.add("active");
         }
 
     });
-},{
-    threshold:0.15
+}, {
+    threshold: 0.15
 });
 
-reveal_bas.forEach(reveal=>{
+reveal_bas.forEach(reveal => {
     observerses.observe(reveal);
 });
 
 // Animation des cartes d'intervents
-const cardes = document.querySelectorAll(".speaker");
+const cardes = document.querySelectorAll(".speaker, .speakere");
 
-const observer = new IntersectionObserver((entries)=>{
+const observer = new IntersectionObserver((entries) => {
 
-    entries.forEach(entry=>{
+    entries.forEach(entry => {
 
-        if(entry.isIntersecting){
+        if (entry.isIntersecting) {
 
             entry.target.classList.add("show");
 
@@ -352,37 +350,44 @@ const observer = new IntersectionObserver((entries)=>{
 
     });
 
-},{
-    threshold:0.2
+}, {
+    threshold: 0.2
 });
 
-cardes.forEach((card,index)=>{
+cardes.forEach((card, index) => {
 
-    card.style.transition=`all .8s ease ${index*0.15}s`;
+    card.style.transition = `all .8s ease ${index * 0.15}s`;
 
     observer.observe(card);
 
 });
 
-/*Apparition des cartes d'intervenants vedettes les une après les autres*/
+/*Apparition des cartes (pourquoi participer) les une après les autres*/
 const revealse = document.querySelectorAll(".reveal");
 
-const observerse = new IntersectionObserver((entries)=>{
-    entries.forEach(entry=>{
+const observerse = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
 
-        if(entry.isIntersecting){
+        if (entry.isIntersecting) {
 
             const delay = entry.target.dataset.delay || 0;
 
-            setTimeout(()=>{
+            setTimeout(() => {
                 entry.target.classList.add("active");
             }, delay);
 
         }
 
     });
-},{
-    threshold:0.2
+}, {
+    threshold: 0.2
 });
-revealse.forEach(item=>observerse.observe(item));
+revealse.forEach(item => observerse.observe(item));
 
+//Anne dynamique
+const anné = document.getElementById("year");
+if (anné) {
+    anné.textContent = new Date().getFullYear();
+}
+
+//three.js de ma page d'intervenants
